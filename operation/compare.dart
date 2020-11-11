01620703cd808a0709e994b715137f8980bf6835
LollipopKit <2036293523@qq.com>
2020-11-11 14:34:07 +0800

    fix compare

--- operation/compare.dart
+++ operation/compare.dart
@@ -6,19 +6,19 @@ bool eq(LuaValue a, LuaValue b){
   if(aa == null) return b == null;
   if(aa is bool) return aa == bb;
   if(aa is String) return aa == bb;
-  if(aa is int) bb is int ? aa == bb : aa.toDouble() == bb;
-  if(aa is double) bb is double ? aa == bb : aa == bb.toDouble();
+  if(aa is int) return bb is int ? aa == bb : aa.toDouble() == bb;
+  if(aa is double) return bb is double ? aa == bb : aa == bb.toDouble();
   return a == b;
 }
 
 bool lt(LuaValue a, LuaValue b){
   dynamic aa = a.luaValue;
   dynamic bb = b.luaValue;
-  if(aa is String) bb is String
+  if(aa is String) return bb is String
       ? (aa.compareTo(bb) == -1 ? true : false)
       : throw UnsupportedError('Unsupported comparision between String and ${bb.runtimeType}');
-  if(aa is int) bb is int ? aa < bb : aa.toDouble() < bb;
-  if(aa is double) bb is double ? aa < bb : aa < bb.toDouble();
+  if(aa is int) return bb is int ? aa < bb : aa.toDouble() < bb;
+  if(aa is double) return bb is double ? aa < bb : aa < bb.toDouble();
   throw UnsupportedError('Unsupported comparison between '
       '${aa.runtimeType} and ${bb.runtimeType}');
 }
@@ -26,11 +26,9 @@ bool lt(LuaValue a, LuaValue b){
 bool le(LuaValue a, LuaValue b){
   dynamic aa = a.luaValue;
   dynamic bb = b.luaValue;
-  if(aa is String) bb is String
-      ? (aa.compareTo(bb) < 1 ? true : false)
-      : throw UnsupportedError('Unsupported comparision between String and ${bb.runtimeType}');
-  if(aa is int) bb is int ? aa <= bb : aa.toDouble() <= bb;
-  if(aa is double) bb is double ? aa <= bb : aa <= bb.toDouble();
+  if(aa is String) return aa.compareTo(bb is String ? bb : bb) < 1 ? true : false;
+  if(aa is int) return bb is int ? aa <= bb : aa.toDouble() <= bb;
+  if(aa is double) return bb is double ? aa <= bb : aa <= bb.toDouble();
   throw UnsupportedError('Unsupported comparison between '
       '${aa.runtimeType} and ${bb.runtimeType}');
 }
